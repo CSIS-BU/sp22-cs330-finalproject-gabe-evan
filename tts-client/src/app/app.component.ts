803530b7f7c4b786e71dcc7933a17bc0d73acc34
Evan Wieland <evwiel1@gmail.com>
2022-04-07 13:35:24 -0500

    Saving progress

--- tts-client/src/app/app.component.ts
+++ tts-client/src/app/app.component.ts
@@ -7,7 +7,16 @@ import {TtsService} from "./tts.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  private board = new Array(3).fill(new Array(3).fill(-1));
+
   constructor(private ttsService: TtsService) {
+
+  }
+
+  placeTic(row: number, col: number) {
+    if(this.board[row][col] < 0){
+      this.ttsService.send({row, col});
+    }
   }
 
   ngOnInit() {
